a07e1ce546605a6cb53c56077c23b62db75fd917
caden.reynolds <caden.reynolds@spaceforce.mil>
2021-10-13 01:39:32 +0200

    made cors everywhere

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ const port = process.env.PORT || 9001
 
 const app = express()
 
-app.use(cors({
-    origin: ['https://apptletter-frontend.herokuapp.com/']
-}))
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
