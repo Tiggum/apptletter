aa0d8deadd9fe329f7ce47137257a8657559f13a
caden.reynolds <caden.reynolds@us.af.mil>
2021-11-05 14:38:14 +0100

    moved font out of body

--- createLetter.js
+++ createLetter.js
@@ -8,15 +8,15 @@ const createLetter = (data, dataCallback, endCallback) => {
     let doc = new PDFDocument({size: 'Letter', margins: {top: .63*72, bottom: 72, left: 72, right: 72}})
     doc.registerFont('Trebuchet', 'fonts/Trebuchet MS/TREBUC.TTF')
     doc.registerFont('Header', 'fonts/Copperplate Gothic/COPRGTB.TTF')
-    data.body.font = data.body.font || 'Times-Roman'
+    data.font = data.font || 'Times-Roman'
 
 
     doc.on('data', dataCallback)
     doc.on('end', endCallback)
     fontSize = data.body.fontSize
     generateHeader(data.header, doc)
-    generateAddress(data.body, doc)
-    generateBody(data.body, doc)
+    generateAddress(data.body, doc, data.font)
+    generateBody(data.body, doc, data.font)
     generateSignatureBlock(data.signature, doc, data.body)
     generateFooter(data.footer, doc)
 
@@ -50,7 +50,7 @@ const generateFooter = (footer, doc) => {
         .text(footer, center, doc.page.height - 63, {lineBreak: false} )
 }
 
-const generateSignatureBlock = (signer, doc, body) => {
+const generateSignatureBlock = (signer, doc, font) => {
     const topLine = `${signer.name.toUpperCase()}, ${signer.rank}, ${signer.branch.toUpperCase()}`
     const spaceWidth = (7.5 * 72) - (4.5*72)
     const textWidth = doc.widthOfString(topLine)
@@ -65,7 +65,7 @@ const generateSignatureBlock = (signer, doc, body) => {
     } else {
         doc
         .moveDown(3)
-        .font(body.font)
+        .font(font)
         .fillColor('black')
         .text(
             topLine,
@@ -75,9 +75,9 @@ const generateSignatureBlock = (signer, doc, body) => {
     }
 }
 
-const generateAddress = (body, doc) => {
+const generateAddress = (body, doc, font) => {
     doc
-        .font(body.font)
+        .font(font)
         .fillColor('black')
         .fontSize(body.fontSize)
         .text('', 72, 72*2, {align: 'right'})
@@ -89,7 +89,7 @@ const generateAddress = (body, doc) => {
         .text(`SUBJECT:  ${body.subject}`)
 }
 
-const generateBody = (body, doc) => {
+const generateBody = (body, doc, font) => {
     doc.moveDown()
     const indent = doc.widthOfString('2.  ')
     for (let i = 0; i < body.paragraphs.length; i++){
@@ -109,9 +109,9 @@ const generateBody = (body, doc) => {
             }
         
             doc.table(tableJson, {
-                prepareHeader: () => doc.font(body.font).fontSize(fontSize),
+                prepareHeader: () => doc.font(font).fontSize(fontSize),
                 prepareRow: (row, indexColumn, indexRow, rectRow) => {
-                    doc.font(body.font).fontSize(fontSize)
+                    doc.font(font).fontSize(fontSize)
                 }
             })
         }
